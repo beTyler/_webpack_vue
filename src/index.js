6b7fba1ae37ee07bad3aef49b41e8e161614e87e
beTyler <610845131@qq.com>
2022-01-08 10:38:12 +0800

    commmit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
+import printMe from "./print";
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // lodash（目前通过一个 script 引W入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
+  btn.innerHTML = "Clickddd me and check the console!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    print: './src/print.js',
+    index: './src/index.js',
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+     static: './dist'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: '[name].bundle.js',
+    clean: true,
+    publicPath: "/"
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "管理输出"
+    })
+  ],
   module: {
     rules: []
   }
